71fb583794fb7a6c4cde0a00c5ad55e982b0815e
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-09-28 15:43:23 +0200

    remove uncessary comment

--- bjj-camp-frontend/src/App.js
+++ bjj-camp-frontend/src/App.js
@@ -5,24 +5,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-/*
-
-import { useNavigate } from "react-router-dom";
- 
-function Home() {
-  let navigate = useNavigate();
- 
-  return (
-    <div>
-      <h1>This is home page</h1>
-      <button onClick={() => navigate('/dashboard')}>Visit Dashboard</button>
-      
-    </div>
-  );
-}
-
-
-*/
