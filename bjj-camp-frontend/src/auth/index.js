7adcc68f350093ab836e249b3212cd0be2f7e45d
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-03 00:12:05 +0200

    add login function

--- bjj-camp-frontend/src/auth/index.js
+++ bjj-camp-frontend/src/auth/index.js
@@ -15,3 +15,19 @@ export const signup = async (user) => {
     console.log(err);
   }
 };
+
+export const login = async (user) => {
+  try {
+    const response = await fetch(`${API}/signin`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
