3453ad5bb0cefad3296909c3eefa9d5041916c8a
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-03 15:16:37 +0200

    add isAuthenticated function

--- bjj-camp-frontend/src/auth/index.js
+++ bjj-camp-frontend/src/auth/index.js
@@ -53,3 +53,15 @@ export const signout = async (next) => {
     }
   }
 };
+
+
+export const isAuthenticated = () => {
+  if(typeof window == 'undefined') {
+    return false
+  }
+  if(localStorage.getItem('jwt')) {
+    return JSON.parse(localStorage.getItem('jwt'))
+  } else {
+    return false;
+  }
+}
