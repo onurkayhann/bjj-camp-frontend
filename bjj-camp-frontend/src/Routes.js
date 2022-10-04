5b3ea299bfd8e8eb0856222cead349e737f809b3
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-04 19:59:20 +0200

    add a private route to the user dashboard

--- bjj-camp-frontend/src/Routes.js
+++ bjj-camp-frontend/src/Routes.js
@@ -14,7 +14,7 @@ const Routes = () => {
         <Route path='/' exact component={Home} />
         <Route path='/login' exact component={Login} />
         <Route path='/signup' exact component={Signup} />
-        <PrivateRoute path='/dashboard' exact component={Dashboard} />
+        <PrivateRoute path='/user/dashboard' exact component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
