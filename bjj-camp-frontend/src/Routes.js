61ed1a509bb11a270c80871f2cd7e99d5e904ea1
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-04 19:39:14 +0200

    add private route for logged in users, a user dashboard

--- bjj-camp-frontend/src/Routes.js
+++ bjj-camp-frontend/src/Routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Signup from './user/Signup';
 import Login from './user/Login';
 import Home from './core/Home';
+import PrivateRoute from './auth/PrivateRoute';
+import Dashboard from './user/UserDashboard';
 import './Routes.css';
 
 const Routes = () => {
@@ -12,6 +14,7 @@ const Routes = () => {
         <Route path='/' exact component={Home} />
         <Route path='/login' exact component={Login} />
         <Route path='/signup' exact component={Signup} />
+        <PrivateRoute path='/dashboard' exact component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
