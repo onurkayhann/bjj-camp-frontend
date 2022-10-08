25934da1215332bdc57fa72ed5d133af539d6319
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-08 22:27:56 +0200

    add data to the Home page, render information about camps depending on the query

--- bjj-camp-frontend/src/core/Home.js
+++ bjj-camp-frontend/src/core/Home.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
+import { getCamps } from './apiCore';
 
 const Home = () => {
+  const [campsByBook, setCampsByBook] = useState([]);
+  const [campsByArrival, setCampsByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    loadCampsByArrival();
+    loadCampsByBook();
+  }, []);
+
+  const loadCampsByBook = () => {
+    getCamps('booked').then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCampsByBook(data);
+      }
+    });
+  };
+
+  const loadCampsByArrival = () => {
+    getCamps('createdAt').then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCampsByArrival(data);
+      }
+    });
+  };
+
   return (
-    <Layout title='Home page' description='Brazilian Jiu-Jitsu training camps'>
-      ...
+    <Layout title='Home page' description='Brazilian Jiu-Jitsu Training Camps'>
+      {JSON.stringify(campsByBook)}
+      <hr />
+      {JSON.stringify(campsByArrival)}
     </Layout>
   );
 };
