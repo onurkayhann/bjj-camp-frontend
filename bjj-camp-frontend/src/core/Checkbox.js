32e16610ed5071dbf63c8e9cf12b1b790bacf1d1
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-10 15:11:14 +0200

    add useState to make the user filter through camps based on belt degree

--- bjj-camp-frontend/src/core/Checkbox.js
+++ bjj-camp-frontend/src/core/Checkbox.js
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from 'react';
 
 const Checkbox = ({ categories }) => {
+  const [checkbox, setCheckbox] = useState([]);
+
+  const handleToggle = (category) => () => {
+    // return the first index or -1
+    const currentCategoryId = checkbox.indexOf(category);
+    const newCheckboxCategoryId = [...checkbox];
+    // if currently checkbox was not already in the checkbox state > push
+    // else pull / take off
+
+    if (currentCategoryId === -1) {
+      newCheckboxCategoryId.push(category);
+    } else {
+      newCheckboxCategoryId.splice(currentCategoryId, 1);
+    }
+    console.log(newCheckboxCategoryId);
+    setCheckbox(newCheckboxCategoryId);
+  };
+
   return categories.map((category, index) => (
     <li key={index} className='list-unstyled'>
-      <input type='checkbox' className='form-check-input' />
+      <input
+        onChange={handleToggle(category._id)}
+        value={checkbox.indexOf(category._id === -1)}
+        type='checkbox'
+        className='form-check-input'
+      />
       <label className='form-check-label'>{category.name}</label>
     </li>
   ));
