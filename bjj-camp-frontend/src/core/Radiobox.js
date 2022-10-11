595e149fe36d8df3611c1790a612820634c86ce0
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-11 21:55:13 +0200

    map through CampPrices to render appropriate camp based on the price

--- bjj-camp-frontend/src/core/Radiobox.js
+++ bjj-camp-frontend/src/core/Radiobox.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, Fragment } from 'react';
 
-const Radiobox = ({ CampPrices }) => {
+const Radiobox = ({ CampPrices, handleFilters }) => {
   const [value, setValue] = useState(0);
 
-  const handleChange = () => {
-    // To be continued...
+  const handleChange = (e) => {
+    handleFilters(e.target.value);
+    setValue(e.target.value);
   };
 
   return CampPrices.map((price, index) => (
@@ -12,6 +13,7 @@ const Radiobox = ({ CampPrices }) => {
       <input
         onChange={handleChange}
         value={`${price._id}`}
+        name={price}
         type='radio'
         className='mr-2 ml-4'
       />
