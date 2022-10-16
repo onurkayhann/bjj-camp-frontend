69630d53d23552dd125edd9bba51b2d59486706b
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-16 21:12:20 +0200

    logically render a button and bootstrap

--- bjj-camp-frontend/src/core/CampCard.js
+++ bjj-camp-frontend/src/core/CampCard.js
@@ -2,23 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import CampImage from './CampImage';
 
-const CampCard = ({ camp }) => {
+const CampCard = ({ camp, showViewCampButton = true }) => {
+  const showViewButton = (showViewCampButton) => {
+    return (
+      showViewCampButton && (
+        <Link to={`/camp/${camp._id}`} className='mr-2'>
+          <button className='btn btn-outline-warning mt-2 mb-2'>
+            View Camp
+          </button>
+        </Link>
+      )
+    );
+  };
+
   return (
-    <div className='col-4 mb-3'>
-      <div className='card'>
-        <div className='card-header'>{camp.name}</div>
-        <div className='card-body'>
-          <CampImage item={camp} url='camp' />
-          <p>{camp.description.substring(0, 100)}</p>
-          <p>${camp.price}</p>
-          <p>Level: {camp.beltcolor}</p>
-          <Link to={`/camp/${camp._id}`}>
-            <button className='btn btn-outline-primary mt-2 mb-2 mr-2'>
-              View Camp
-            </button>
-          </Link>
-          <button className='btn btn-outline-warning mt-2 mb-2'>Book</button>
-        </div>
+    <div className='card'>
+      <div className='card-header'>{camp.name}</div>
+      <div className='card-body'>
+        <CampImage item={camp} url='camp' />
+        <p>{camp.description.substring(0, 100)}</p>
+        <p>${camp.price}</p>
+        <p>Level: {camp.beltcolor}</p>
+
+        {showViewButton(showViewCampButton)}
+        <button className='btn btn-outline-primary mt-2 mb-2'>Book Camp</button>
       </div>
     </div>
   );
