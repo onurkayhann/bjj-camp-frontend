44b2a0269cb60d76a1ae2d7fc00d54cdc8b89960
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-16 22:02:21 +0200

    show more information about camp

--- bjj-camp-frontend/src/core/CampCard.js
+++ bjj-camp-frontend/src/core/CampCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CampImage from './CampImage';
+import moment from 'moment';
 
 const CampCard = ({ camp, showViewCampButton = true }) => {
   const showViewButton = (showViewCampButton) => {
@@ -15,17 +16,38 @@ const CampCard = ({ camp, showViewCampButton = true }) => {
     );
   };
 
+  const showBookCampButton = () => {
+    return (
+      <button className='btn btn-outline-primary mt-2 mb-2'>Book Camp</button>
+    );
+  };
+
+  const showQuantity = (quantity, booked) => {
+    return quantity > 0 ? (
+      <span className='badge badge-success badge-pill mb-1'>
+        {camp.quantity - camp.booked} spots left
+      </span>
+    ) : (
+      <span className='badge badge-danger badge-pill mb-1'>Fully booked</span>
+    );
+  };
+
   return (
     <div className='card'>
-      <div className='card-header'>{camp.name}</div>
+      <div className='card-header camp-name'>{camp.name}</div>
       <div className='card-body'>
         <CampImage item={camp} url='camp' />
-        <p>{camp.description.substring(0, 100)}</p>
-        <p>${camp.price}</p>
-        <p>Level: {camp.beltcolor}</p>
-
+        <p className='lead mt-2'>{camp.description.substring(0, 100)}</p>
+        <p className='black-10'>${camp.price}</p>
+        <p className='black-9'>Level: {camp.beltcolor}</p>
+        <p className='black-8'>
+          Category: {camp.category && camp.category.name}
+        </p>
+        <p className='black-8'>Added on {moment(camp.createdAt).fromNow()}</p>
+        {showQuantity(camp.quantity)}
+        <br />
         {showViewButton(showViewCampButton)}
-        <button className='btn btn-outline-primary mt-2 mb-2'>Book Camp</button>
+        {showBookCampButton()}
       </div>
     </div>
   );
