ba3b883130a518ac04ed13bebb566d848bd13f68
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-20 00:45:41 +0200

    add getBrainTreeClientToken function to get specific bookings from users

--- bjj-camp-frontend/src/core/apiCore.js
+++ bjj-camp-frontend/src/core/apiCore.js
@@ -75,4 +75,19 @@ export const listRelated = (campId) => {
     .catch((err) => console.log(err));
 };
 
+export const getBrainTreeClientToken = (userId, token) => {
+  return fetch(`${API}/braintree/getToken/${userId}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
 export default getCamps;
