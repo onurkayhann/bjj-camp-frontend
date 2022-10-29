c5cffb57bc925da1c9b1404bd2cdeb43cd233f29
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-29 20:10:48 +0200

    import some stuff, also populate the user information from the backend on the frontend side

--- bjj-camp-frontend/src/user/Profile.js
+++ bjj-camp-frontend/src/user/Profile.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
+import { read, update, updateUser } from './apiUser';
 
-const Profile = () => {
-  return <h2>Profile</h2>;
+const Profile = ({ match }) => {
+  const [values, setValues] = useState({
+    name: '',
+    belt_color: '',
+    password: '',
+    error: false,
+    success: false,
+  });
+
+  const { token } = isAuthenticated();
+  const { name, belt_color, password, error, success } = values;
+
+  const initProfile = (userId) => {
+    read(userId, token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: true });
+      } else {
+        setValues({
+          ...values,
+          name: data.name,
+          belt_color: data.belt_color,
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    initProfile(match.params.userId);
+  }, []);
+
+  return (
+    <Layout
+      title='Profile'
+      description='Update your profile'
+      className='container-fluid'
+    >
+      <h2 className='mb-4'>Profile update</h2>
+      {JSON.stringify(values)}
+    </Layout>
+  );
 };
 
 export default Profile;
