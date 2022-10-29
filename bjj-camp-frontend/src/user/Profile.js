7070d673ce5d57e48ec33f677c19da21ae5deb34
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-29 20:40:43 +0200

    add handleChange, clickSubmit and redirectUser functions, this add will make the user r update its information

--- bjj-camp-frontend/src/user/Profile.js
+++ bjj-camp-frontend/src/user/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { read, update, updateUser } from './apiUser';
 
 const Profile = ({ match }) => {
@@ -34,6 +34,75 @@ const Profile = ({ match }) => {
     initProfile(match.params.userId);
   }, []);
 
+  const handleChange = (name) => (e) => {
+    setValues({ ...values, error: false, [name]: e.target.value });
+  };
+
+  const clickSubmit = (e) => {
+    e.preventDefault();
+    update(match.params.userId, token, { name, belt_color, password }).then(
+      (data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          updateUser(data, () => {
+            setValues({
+              ...values,
+              name: data.name,
+              belt_color: data.belt_color,
+              success: true,
+            });
+          });
+        }
+      }
+    );
+  };
+
+  const redirectUser = (success) => {
+    if (success) {
+      return <Redirect to='/cart' />;
+    }
+  };
+
+  const profileUpdate = (name, belt_color, password) => {
+    return (
+      <form>
+        <div className='form-group'>
+          <label className='text-muted'>Name</label>
+          <input
+            className='form-control'
+            value={name}
+            type='text'
+            onChange={handleChange('name')}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label className='text-muted'>Belt Color</label>
+          <input
+            className='form-control'
+            value={belt_color}
+            type='text'
+            onChange={handleChange('belt_color')}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label className='text-muted'>Password</label>
+          <input
+            className='form-control'
+            value={password}
+            type='password'
+            onChange={handleChange('password')}
+          />
+        </div>
+        <button className='btn btn-primary' onClick={clickSubmit}>
+          Submit
+        </button>
+      </form>
+    );
+  };
+
   return (
     <Layout
       title='Profile'
@@ -41,7 +110,8 @@ const Profile = ({ match }) => {
       className='container-fluid'
     >
       <h2 className='mb-4'>Profile update</h2>
-      {JSON.stringify(values)}
+      {profileUpdate(name, belt_color, password)}
+      {redirectUser(success)}
     </Layout>
   );
 };
