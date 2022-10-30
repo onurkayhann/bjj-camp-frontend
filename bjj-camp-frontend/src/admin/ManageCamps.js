b51e9f463582a554df4bab554f750c56902a2707
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-30 15:18:29 +0100

    import getCamps and deleteCamp into ManageCamps, create a destroy function that will be implemented on onClick so that admin can delete a camp

--- bjj-camp-frontend/src/admin/ManageCamps.js
+++ bjj-camp-frontend/src/admin/ManageCamps.js
@@ -1,9 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
+import { getCamps, deleteCamp } from './apiAdmin';
 
 const ManageCamps = () => {
+  const [camps, setCamps] = useState([]);
+
+  const { user, token } = isAuthenticated();
+
+  const loadCamps = () => {
+    getCamps().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setCamps(data);
+      }
+    });
+  };
+
+  const destroy = (campId) => {
+    deleteCamp(campId, user._id, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        loadCamps();
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadCamps();
+  }, []);
+
   return (
     <Layout
       title='Manage camps'
@@ -11,7 +40,28 @@ const ManageCamps = () => {
       className='container-fluid'
     >
       <div className='row'>
-        <div>Soon...</div>
+        <div className='col-12'>
+          <ul className='list-group'>
+            {camps.map((c, index) => (
+              <li
+                key={index}
+                className='list-group-item d-flex justify-content-between align-items-center'
+              >
+                <strong>{c.name}</strong>
+                <Link to={`/admin/camp/update/${c._id}`}>
+                  <span className='badge badge-warning badge-pill'>Update</span>
+                </Link>
+                <span
+                  onClick={() => destroy(c._id)}
+                  className='badge badge-danger'
+                  style={{ cursor: 'pointer' }}
+                >
+                  Delete
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </Layout>
   );
